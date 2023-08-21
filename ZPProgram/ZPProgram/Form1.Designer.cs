517a583571696da15db329ecd361dc76f068fb0f
Patrik Dvorak <dvorak07patrik@seznam.cz>
2023-08-21 19:37:26 +0200

    small form change

--- ZPProgram/ZPProgram/Form1.Designer.cs
+++ ZPProgram/ZPProgram/Form1.Designer.cs
@@ -29,12 +29,12 @@
         private void InitializeComponent()
         {
             this.leftSideLength = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.LeftSideLengthLabel = new System.Windows.Forms.Label();
+            this.leftSideLabel = new System.Windows.Forms.Label();
             this.leftSideHeight = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.rightSideLabel = new System.Windows.Forms.Label();
+            this.RightSideLengthLabel = new System.Windows.Forms.Label();
             this.rigthSideLength = new System.Windows.Forms.NumericUpDown();
             this.rightSideHeight = new System.Windows.Forms.NumericUpDown();
             this.label6 = new System.Windows.Forms.Label();
@@ -57,38 +57,38 @@
             0,
             0,
             0});
-            this.leftSideLength.Location = new System.Drawing.Point(10, 70);
-            this.leftSideLength.Margin = new System.Windows.Forms.Padding(4);
+            this.leftSideLength.Location = new System.Drawing.Point(17, 86);
+            this.leftSideLength.Margin = new System.Windows.Forms.Padding(7);
             this.leftSideLength.Maximum = new decimal(new int[] {
             15000,
             0,
             0,
             0});
             this.leftSideLength.Name = "leftSideLength";
-            this.leftSideLength.Size = new System.Drawing.Size(160, 22);
+            this.leftSideLength.Size = new System.Drawing.Size(280, 28);
             this.leftSideLength.TabIndex = 0;
             // 
-            // label1
+            // LeftSideLengthLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(8, 48);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 18);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Délka levé strany:";
+            this.LeftSideLengthLabel.AutoSize = true;
+            this.LeftSideLengthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.LeftSideLengthLabel.Location = new System.Drawing.Point(12, 54);
+            this.LeftSideLengthLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.LeftSideLengthLabel.Name = "LeftSideLengthLabel";
+            this.LeftSideLengthLabel.Size = new System.Drawing.Size(133, 20);
+            this.LeftSideLengthLabel.TabIndex = 1;
+            this.LeftSideLengthLabel.Text = "Délka levé strany:";
             // 
-            // label2
+            // leftSideLabel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(4, 0);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 34);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Levá strana";
+            this.leftSideLabel.AutoSize = true;
+            this.leftSideLabel.Font = new System.Drawing.Font("Bahnschrift Condensed", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.leftSideLabel.Location = new System.Drawing.Point(10, 0);
+            this.leftSideLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.leftSideLabel.Name = "leftSideLabel";
+            this.leftSideLabel.Size = new System.Drawing.Size(119, 33);
+            this.leftSideLabel.TabIndex = 2;
+            this.leftSideLabel.Text = "Levá strana";
             // 
             // leftSideHeight
             // 
@@ -99,46 +99,48 @@
             0,
             0,
             0});
-            this.leftSideHeight.Location = new System.Drawing.Point(22, 102);
-            this.leftSideHeight.Margin = new System.Windows.Forms.Padding(4);
+            this.leftSideHeight.Location = new System.Drawing.Point(38, 185);
+            this.leftSideHeight.Margin = new System.Windows.Forms.Padding(7);
             this.leftSideHeight.Maximum = new decimal(new int[] {
             4000,
             0,
             0,
             0});
             this.leftSideHeight.Name = "leftSideHeight";
-            this.leftSideHeight.Size = new System.Drawing.Size(81, 22);
+            this.leftSideHeight.Size = new System.Drawing.Size(142, 28);
             this.leftSideHeight.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 82);
+            this.label3.Location = new System.Drawing.Point(33, 149);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(116, 16);
+            this.label3.Size = new System.Drawing.Size(159, 24);
             this.label3.TabIndex = 4;
             this.label3.Text = "Výška levé strany:";
             // 
-            // label4
+            // rightSideLabel
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Bahnschrift Condensed", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(4, 105);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(134, 34);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Pravá strana";
+            this.rightSideLabel.AutoSize = true;
+            this.rightSideLabel.Font = new System.Drawing.Font("Bahnschrift Condensed", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.rightSideLabel.Location = new System.Drawing.Point(11, 151);
+            this.rightSideLabel.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.rightSideLabel.Name = "rightSideLabel";
+            this.rightSideLabel.Size = new System.Drawing.Size(129, 33);
+            this.rightSideLabel.TabIndex = 5;
+            this.rightSideLabel.Text = "Pravá strana";
             // 
-            // label5
+            // RightSideLengthLabel
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label5.Location = new System.Drawing.Point(8, 148);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(134, 18);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "Délka pravé strany:";
+            this.RightSideLengthLabel.AutoSize = true;
+            this.RightSideLengthLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.RightSideLengthLabel.Location = new System.Drawing.Point(12, 198);
+            this.RightSideLengthLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.RightSideLengthLabel.Name = "RightSideLengthLabel";
+            this.RightSideLengthLabel.Size = new System.Drawing.Size(144, 20);
+            this.RightSideLengthLabel.TabIndex = 6;
+            this.RightSideLengthLabel.Text = "Délka pravé strany:";
             // 
             // rigthSideLength
             // 
@@ -149,15 +151,15 @@
             0,
             0,
             0});
-            this.rigthSideLength.Location = new System.Drawing.Point(10, 170);
-            this.rigthSideLength.Margin = new System.Windows.Forms.Padding(4);
+            this.rigthSideLength.Location = new System.Drawing.Point(16, 225);
+            this.rigthSideLength.Margin = new System.Windows.Forms.Padding(7);
             this.rigthSideLength.Maximum = new decimal(new int[] {
             15000,
             0,
             0,
             0});
             this.rigthSideLength.Name = "rigthSideLength";
-            this.rigthSideLength.Size = new System.Drawing.Size(160, 22);
+            this.rigthSideLength.Size = new System.Drawing.Size(280, 28);
             this.rigthSideLength.TabIndex = 7;
             // 
             // rightSideHeight
@@ -169,54 +171,58 @@
             0,
             0,
             0});
-            this.rightSideHeight.Location = new System.Drawing.Point(22, 207);
-            this.rightSideHeight.Margin = new System.Windows.Forms.Padding(4);
+            this.rightSideHeight.Location = new System.Drawing.Point(38, 375);
+            this.rightSideHeight.Margin = new System.Windows.Forms.Padding(7);
             this.rightSideHeight.Maximum = new decimal(new int[] {
             4000,
             0,
             0,
             0});
             this.rightSideHeight.Name = "rightSideHeight";
-            this.rightSideHeight.Size = new System.Drawing.Size(81, 22);
+            this.rightSideHeight.Size = new System.Drawing.Size(142, 28);
             this.rightSideHeight.TabIndex = 9;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(19, 187);
+            this.label6.Location = new System.Drawing.Point(33, 339);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(125, 16);
+            this.label6.Size = new System.Drawing.Size(171, 24);
             this.label6.TabIndex = 10;
             this.label6.Text = "Výška pravé strany:";
             // 
             // splitContainer1
             // 
-            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Location = new System.Drawing.Point(21, 22);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(5);
             this.splitContainer1.Name = "splitContainer1";
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.label2);
-            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.leftSideLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.LeftSideLengthLabel);
             this.splitContainer1.Panel1.Controls.Add(this.leftSideLength);
             this.splitContainer1.Panel1.Controls.Add(this.rigthSideLength);
-            this.splitContainer1.Panel1.Controls.Add(this.label4);
-            this.splitContainer1.Panel1.Controls.Add(this.label5);
-            this.splitContainer1.Size = new System.Drawing.Size(438, 569);
-            this.splitContainer1.SplitterDistance = 216;
+            this.splitContainer1.Panel1.Controls.Add(this.rightSideLabel);
+            this.splitContainer1.Panel1.Controls.Add(this.RightSideLengthLabel);
+            this.splitContainer1.Size = new System.Drawing.Size(766, 704);
+            this.splitContainer1.SplitterDistance = 377;
+            this.splitContainer1.SplitterWidth = 7;
             this.splitContainer1.TabIndex = 11;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1099, 593);
+            this.ClientSize = new System.Drawing.Size(1082, 753);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.rightSideHeight);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.leftSideHeight);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Form1";
             this.Text = "Kalkulátor zimní zahrady";
             ((System.ComponentModel.ISupportInitialize)(this.leftSideLength)).EndInit();
@@ -235,12 +241,12 @@
         #endregion
 
         private System.Windows.Forms.NumericUpDown leftSideLength;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LeftSideLengthLabel;
+        private System.Windows.Forms.Label leftSideLabel;
         private System.Windows.Forms.NumericUpDown leftSideHeight;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label rightSideLabel;
+        private System.Windows.Forms.Label RightSideLengthLabel;
         private System.Windows.Forms.NumericUpDown rigthSideLength;
         private System.Windows.Forms.NumericUpDown rightSideHeight;
         private System.Windows.Forms.Label label6;
