583d97e4fae906afe887dd1ee83f49573fe0f99f
Patrik Dvorak <dvorak07patrik@seznam.cz>
2023-08-26 15:27:09 +0200

    add Side.cs class and create Side objects on button click

--- ZPProgram/ZPProgram/Form1.cs
+++ ZPProgram/ZPProgram/Form1.cs
@@ -17,5 +17,11 @@ namespace ZPProgram
             InitializeComponent();
         }
 
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            Side leftSide = new Side(Convert.ToInt32(leftSideLength.Value), Convert.ToInt32(leftSideHeight.Value), Convert.ToInt32(leftSideDoorCount.Value));
+            Side rightSide = new Side(Convert.ToInt32(rightSideLength.Value), Convert.ToInt32(rightSideHeight.Value), Convert.ToInt32(rightSideDoorCount.Value));
+            Side midSide = new Side(Convert.ToInt32(midSideLength.Value), Convert.ToInt32(midSideHeight.Value), Convert.ToInt32(midSideDoorCount.Value));
+        }
     }
 }
---
+++ ZPProgram/ZPProgram/Side.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPProgram
+{
+    internal class Side
+    {
+        public int Length { get; set; }
+        public int Height { get; set; }
+        public int NumberOfDoors { get; set; }
+
+        public Side(int length, int height, int numberOfDoors) 
+        { 
+            Length = length;
+            Height = height;
+            NumberOfDoors = numberOfDoors;
+        }
+    }
+}
