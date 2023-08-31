ae5086bd243c3a80df1201ec4059a9595bc822cf
Patrik Dvorak <dvorak07patrik@seznam.cz>
2023-08-31 21:24:36 +0200

    edit side parts visualization

--- ZPProgram/ZPProgram/Form2.Designer.cs
+++ ZPProgram/ZPProgram/Form2.Designer.cs
@@ -28,20 +28,39 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.errorLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.AutoSize = true;
+            this.errorLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.errorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.errorLabel.ForeColor = System.Drawing.Color.Red;
+            this.errorLabel.Location = new System.Drawing.Point(13, 13);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.errorLabel.Size = new System.Drawing.Size(378, 25);
+            this.errorLabel.TabIndex = 0;
+            this.errorLabel.Text = "Žádná strana nemůže být vykreslena !";
+            this.errorLabel.Visible = false;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1477, 876);
+            this.Controls.Add(this.errorLabel);
             this.Name = "Form2";
             this.Text = "Form2";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form2_Paint);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label errorLabel;
     }
 }
--- ZPProgram/ZPProgram/Form2.cs
+++ ZPProgram/ZPProgram/Form2.cs
@@ -32,6 +32,11 @@ namespace ZPProgram
         // paints 2D visualisation of sides
         private void Form2_Paint(object sender, PaintEventArgs e)
         {
+            // without this condition are the last two conditions invalid (Form2_Paint runs twice sometimes)
+            if (Size.Width != 1493) { return; }
+
+
+            // painting valid sides and decreasing form size if a side is not printed
             int start = 30;
             Font font = new Font("Microsoft Sans Serif", 26, FontStyle.Bold, GraphicsUnit.Pixel);
             if (LeftSide != null)
@@ -41,6 +46,7 @@ namespace ZPProgram
                 PrintSide(LeftSide, leftSidePartsValues, start, e);
                 start += 500;
             }
+            else { Size = new Size(Size.Width - 500, Size.Height); }
             if (MidSide != null)
             {
                 e.Graphics.DrawString("Prostřední strana - " + MidSide.length.ToString() + "mm", font, Brushes.Black, start, 30);
@@ -48,6 +54,7 @@ namespace ZPProgram
                 PrintSide(MidSide, midSidePartsValues, start, e);
                 start += 500;
             }
+            else { Size = new Size(Size.Width - 500, Size.Height); }
             if (RightSide != null)
             {
                 e.Graphics.DrawString("Pravá strana - " + RightSide.length.ToString() + "mm", font, Brushes.Black, start, 30);
@@ -55,17 +62,16 @@ namespace ZPProgram
                 PrintSide(RightSide, rightSidePartsValues, start, e);
                 start += 500;
             }
-            // Graphics g = e.Graphics;
-            // Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
-            // Brush brush = new SolidBrush(Color.Blue);
-            // e.Graphics.DrawLine(pen, 20, 10, 300, 10);
-            // e.Graphics.DrawRectangle(pen, 30, 30, 10, 230);
-            // e.Graphics.FillRectangle(brush, 30, 30, 10, 230);
+            else if (Size.Width < 500) 
+            { 
+                errorLabel.Visible = true;
+            }
+            else { Size = new Size(Size.Width - 500, Size.Height); }
         }
 
         private void PrintSide(Side side, ((int count, int lenght) HL, (int count, int lenght) V, (int count, int lenght) VM, (int count, int lenght) HS, (int count, int lenght) BS, (int count, int lenght) VA, (int width, int height) glass) sidePartsValues, int start, PaintEventArgs e)
         {
-
+            // declaration of pen, brushes and font variables
             Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
             Brush blueBrush = new SolidBrush(Color.Blue);
             Brush greyBrush = new SolidBrush(Color.Gray);
@@ -79,43 +85,34 @@ namespace ZPProgram
             e.Graphics.DrawRectangle(pen, start + 15, topStart + 55, sidePartsValues.HL.lenght / 10, HLHeight);
             e.Graphics.FillRectangle(greyBrush, start + 15, topStart + 55, sidePartsValues.HL.lenght / 10, HLHeight);
 
-            
-
-
-
-
+            // V Count + Length with visualization
             topStart += 75;
             start -= 80;
-            // V Count + Length with visualization 
             e.Graphics.DrawString("V - " + sidePartsValues.V.count.ToString() + "ks", font1, Brushes.Black, start, ((sidePartsValues.V.lenght / 10) / 2 + (topStart - 25)));
             e.Graphics.DrawString(sidePartsValues.V.lenght.ToString() + "mm", font1, Brushes.Black, start, ((sidePartsValues.V.lenght / 10) / 2 + (topStart + 5)));
-            //e.Graphics.DrawLine(pen, 30, ((sidePartsValues.V.VLenght / 10) / 2 + 30), 200, ((sidePartsValues.V.VLenght / 10) / 2 + 30));
             start += 80;
             e.Graphics.DrawRectangle(pen, start, topStart, VWidth, sidePartsValues.V.lenght / 10);
             e.Graphics.FillRectangle(greyBrush, start, topStart, VWidth, sidePartsValues.V.lenght / 10);
 
-            // down HL vizualization + glass width
+            // down HL visualization + glass width
             e.Graphics.DrawRectangle(pen, start + 15, topStart + 5 + (sidePartsValues.V.lenght / 10), sidePartsValues.HL.lenght / 10, HLHeight);
             e.Graphics.FillRectangle(greyBrush, start + 15, topStart + 5 + (sidePartsValues.V.lenght / 10), sidePartsValues.HL.lenght / 10, HLHeight);
-            e.Graphics.DrawString("sklo - " + (sidePartsValues.HL.count / 2).ToString() + "ks", font1, Brushes.Blue, start + 15, topStart + (sidePartsValues.V.lenght / 10) + 30);
-            e.Graphics.DrawString(sidePartsValues.glass.width.ToString() + "mm", font1, Brushes.Blue, start + 15, topStart + (sidePartsValues.V.lenght / 10) + 60);
+            e.Graphics.DrawString("sklo - " + (sidePartsValues.HL.count / 2).ToString() + "ks", font1, blueBrush, start + 15, topStart + (sidePartsValues.V.lenght / 10) + 30);
+            e.Graphics.DrawString(sidePartsValues.glass.width.ToString() + "mm", font1, blueBrush, start + 15, topStart + (sidePartsValues.V.lenght / 10) + 60);
 
 
             // VM Count + Length with visualization + glass height
             start += 10;
             e.Graphics.DrawRectangle(pen, start + 10 + (sidePartsValues.glass.width / 10), topStart, VWidth, sidePartsValues.V.lenght / 10);
             e.Graphics.FillRectangle(greyBrush, start + 10 + (sidePartsValues.glass.width / 10), topStart, VWidth, sidePartsValues.V.lenght / 10);
-            e.Graphics.DrawString("VM - " + sidePartsValues.VM.count.ToString() + "ks", font1, Brushes.Black, start + 30 + (sidePartsValues.glass.width / 10), ((sidePartsValues.VM.lenght / 10) / 2 + (topStart - 25)));
-            e.Graphics.DrawString(sidePartsValues.VM.lenght.ToString() + "mm", font1, Brushes.Black, start + 30 + (sidePartsValues.glass.width / 10), ((sidePartsValues.VM.lenght / 10) / 2 + (topStart + 5)));
-            e.Graphics.DrawString("sklo - " + (sidePartsValues.HL.count / 2).ToString() + "ks", font1, Brushes.Blue, start + 30 + (sidePartsValues.glass.width / 10), ((sidePartsValues.V.lenght / 10) / 2 + (topStart + 35)));
-            e.Graphics.DrawString(sidePartsValues.glass.height.ToString() + "mm", font1, Brushes.Blue, start + 30 + (sidePartsValues.glass.width / 10), ((sidePartsValues.V.lenght / 10) / 2 + (topStart + 65)));
+            e.Graphics.DrawString("VM - " + sidePartsValues.VM.count.ToString() + "ks", font1, Brushes.Black, start + 30 + (sidePartsValues.glass.width / 10), ((sidePartsValues.VM.lenght / 10) / 2 + (topStart - 55)));
+            e.Graphics.DrawString(sidePartsValues.VM.lenght.ToString() + "mm", font1, Brushes.Black, start + 30 + (sidePartsValues.glass.width / 10), ((sidePartsValues.VM.lenght / 10) / 2 + (topStart - 25)));
+            e.Graphics.DrawString("sklo - " + (sidePartsValues.HL.count / 2).ToString() + "ks", font1, blueBrush, start + 30 + (sidePartsValues.glass.width / 10), ((sidePartsValues.V.lenght / 10) / 2 + (topStart + 5)));
+            e.Graphics.DrawString(sidePartsValues.glass.height.ToString() + "mm", font1, blueBrush, start + 30 + (sidePartsValues.glass.width / 10), ((sidePartsValues.V.lenght / 10) / 2 + (topStart + 35)));
 
-            // Glass width + height with visualization
+            // Glass visualization
             e.Graphics.DrawRectangle(pen, start + 5, topStart + 5, sidePartsValues.glass.width / 10, sidePartsValues.glass.height / 10);
             e.Graphics.FillRectangle(blueBrush, start + 5, topStart + 5, sidePartsValues.glass.width / 10, sidePartsValues.glass.height / 10);
-            //e.Graphics.DrawString("sklo", font1, Brushes.Black, start + 5, ((sidePartsValues.VM.lenght / 10) / 2 + (topStart - 25)));
-            //e.Graphics.DrawString((sidePartsValues.HL.count / 2).ToString() + "ks", font1, Brushes.Black, start + 5, ((sidePartsValues.VM.lenght / 10) / 2 + (topStart + 5)));
-
 
         }
     }
