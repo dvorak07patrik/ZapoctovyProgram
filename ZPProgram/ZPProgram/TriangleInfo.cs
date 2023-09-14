217c98bc6c2d2293cfc9f70a7350dd3dce9a0aa3
Patrik Dvorak <dvorak07patrik@seznam.cz>
2023-09-14 16:42:20 +0200

    add class for calculating and storing glass triangle dimensions

---
+++ ZPProgram/ZPProgram/TriangleInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPProgram
+{
+    internal class TriangleInfo
+    {
+        public double Hypotenuse { get; set; }
+        public double AngleAlpha { get; set; }
+        public double AngleBeta { get; set; }
+        public double ShorterLeg { get; set; }
+        public double LongerLeg { get; set; }
+
+        public TriangleInfo(double shorterLeg, double longerLeg)
+        {
+            ShorterLeg = shorterLeg;
+            LongerLeg = longerLeg;
+        }
+
+        public void CalculateTriangle()
+        {
+            Hypotenuse = Math.Sqrt(ShorterLeg * ShorterLeg + LongerLeg * LongerLeg);
+
+            // left upper
+            AngleAlpha = Math.Atan2(LongerLeg, ShorterLeg) * (180.0 / Math.PI);
+
+            // right button
+            AngleBeta = Math.Atan2(ShorterLeg, LongerLeg) * (180.0 / Math.PI);
+        }
+    }
+}
