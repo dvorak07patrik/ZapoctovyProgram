d3ceb78b21ae8dc2b34a2cc19750279bbecd02e3
Patrik Dvorak <dvorak07patrik@seznam.cz>
2023-09-14 16:43:09 +0200

    add class for calculating and storing Rafters dimensions and angle

---
+++ ZPProgram/ZPProgram/RafterInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPProgram
+{
+    internal class RafterInfo
+    {
+        public double Length { get; set; }
+        public double Angle { get; set; }
+        public int Count { get; set; }
+        public double RafterSpacing { get; set; }
+        public RafterInfo(double length, double angle) 
+        {
+            Length = length;
+            Angle = angle;
+        }
+
+        public void CalculateRafter(double gardenWidth)
+        {
+            Count = Convert.ToInt32((gardenWidth / 1000) + 1);
+            if (Count < 2) { Count = 2; }
+            RafterSpacing = (gardenWidth - (100 * Count)) / (Count - 1);
+        }
+    }
+}
