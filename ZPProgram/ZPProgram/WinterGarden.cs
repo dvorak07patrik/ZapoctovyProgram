ebc938291e2808bc717bf6ed1341f17ae8c4eec5
Patrik Dvorak <dvorak07patrik@seznam.cz>
2023-09-14 16:41:51 +0200

    add class to store Winter Garden dimensions

---
+++ ZPProgram/ZPProgram/WinterGarden.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZPProgram
+{
+    public class WinterGarden
+    {
+        public double Length;
+        public double Width;
+        public double BackHeight;
+        public double FrontHeight;
+        public TriangleInfo Triangle;
+        public RafterInfo Rafter;
+        public RoofGlassInfo RoofGlass;
+
+
+        public WinterGarden(double length, double width, double backHeight, double frontHeight) 
+        {
+            Length = length;
+            Width = width;
+            BackHeight = backHeight;
+            FrontHeight = frontHeight + 100;
+        }
+
+        public void CalculateRoof()
+        {
+            // calculation of glass triangle
+            // minus upper rafter
+            double shortLeg = BackHeight - 100 - FrontHeight;
+            double longLeg = Length - 200;
+            Triangle = new TriangleInfo(shortLeg, longLeg);
+            Triangle.CalculateTriangle();
+
+            Rafter = new RafterInfo(Triangle.Hypotenuse, Triangle.AngleBeta);
+            Rafter.CalculateRafter(Width);
+
+            RoofGlass = new RoofGlassInfo(Rafter.Count, Rafter.Length);
+            RoofGlass.CalculateGlass(Rafter.RafterSpacing);
+
+
+            // return (triangle.Hypotenuse, triangle.)
+        }
+
+        
+    }
+}
